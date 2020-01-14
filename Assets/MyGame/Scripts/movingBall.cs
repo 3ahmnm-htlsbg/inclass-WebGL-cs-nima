19e86425fcc4b3db99714677315fefc9b375e588
maringernina <nina.maringer01@htl-salzburg.ac.at>
2020-01-14 13:52:43 +0100

    add Assets folder

---
+++ Assets/MyGame/Scripts/movingBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movingBall : MonoBehaviour
+{
+
+    public GameObject ball;
+    
+    [SerializeField]private GameObject[] ballPositions;
+
+
+    private int RandomNum()
+    {
+        return Random.Range(0, 4);
+    }
+ 
+
+    public void BallPos()
+    { 
+        ball.transform.position = ballPositions[RandomNum()].transform.position;
+    }
+
+}
